097fc8bbfc43a487b24bf207010611beb0abdfdf
Konstantin Grigorev <info@a6raywa1cher.com>
2022-12-17 16:02:50 +0300

    rewrite GraphServiceImpl.convertToVisibilityGraph

--- visible-graph-finder/src/main/kotlin/com/a6raywa1cher/graph/GraphServiceImpl.kt
+++ visible-graph-finder/src/main/kotlin/com/a6raywa1cher/graph/GraphServiceImpl.kt
@@ -2,10 +2,9 @@ package com.a6raywa1cher.graph
 
 import org.slf4j.LoggerFactory
 import java.lang.Math.floorMod
-import kotlin.math.acos
 import kotlin.math.sqrt
 
-private data class Vector(val x: Int, val y: Int) {
+internal data class Vector(val x: Int, val y: Int) {
     constructor(from: Point, to: Point) : this(to.x - from.x, to.y - from.y)
 
     fun length(): Double = sqrt((x * x + y * y).toDouble())
@@ -15,79 +14,100 @@ private data class Vector(val x: Int, val y: Int) {
     infix fun dot(other: Vector): Int = this.x * other.x + this.y * other.y
 }
 
+internal fun Point.isInsideObservationArea(pl: Point, p: Point, pr: Point): Boolean {
+    val v1 = Vector(p, pl)
+    val v2 = Vector(p, pr)
+    val v3 = Vector(p, this)
+    if (v1 * v2 == 0) return false
+    return if (v1 * v2 < 0) v1 * v3 < 0 && v3 * v2 < 0 else v1 * v3 > 0 && v3 * v2 > 0
+}
+
+internal fun getPrevIndex(i: Int, list: List<*>) = floorMod(i - 1, list.size)
+
+internal fun getNextIndex(i: Int, list: List<*>) = floorMod(i + 1, list.size)
+
+internal fun Polygon.getConvexPoints(): List<Point> {
+    if (points.size < 3) return points
+    val out = mutableListOf<Point>()
+    for (i in points.indices) {
+        val pl = points[getPrevIndex(i, points)]
+        val p = points[i]
+        val pr = points[getNextIndex(i, points)]
+        if (Vector(pl, pr) * Vector(pl, p) >= 0) out.add(p)
+    }
+    return out
+}
+
+// https://stackoverflow.com/a/18472899
+internal fun Polygon.isClockwise(): Boolean {
+    var sum = 0.0
+    for (i in points.indices) {
+        val p1 = points[i]
+        val p2 = points[getNextIndex(i, points)]
+        sum += (p2.x - p1.x) * (p2.y + p1.y)
+    }
+    return sum < 0 // reversed Y axis
+}
+
+internal fun Polygon.normalize() =
+    if (isClockwise()) Polygon(points.reversed()) else this
+
+internal fun Polygon.lines() = sequence {
+    for (i in points.indices) {
+        val p1 = points[i]
+        val p2 = points[getNextIndex(i, points)]
+        yield(Pair(p1, p2))
+    }
+}
+
+private fun vectorMultiplication(a: Point, b: Point, origin: Point): Int = Vector(origin, a) * Vector(origin, b)
+
+internal fun intersects(p11: Point, p12: Point, p21: Point, p22: Point): Boolean {
+    val t1 = vectorMultiplication(p22, p12, p11)
+    val t2 = vectorMultiplication(p21, p12, p11)
+    val d1 = vectorMultiplication(p22, p12, p21)
+    val d2 = vectorMultiplication(p22, p11, p21)
+    return t1 * t2 <= 0 && d1 * d2 <= 0
+}
+
 class GraphServiceImpl : GraphService {
     private val logger = LoggerFactory.getLogger(javaClass)
 
-    private fun vectorMultiplication(a: Point, b: Point, origin: Point): Int = Vector(origin, a) * Vector(origin, b)
+    override fun convertToVisibilityGraph(areaMap: AreaMap): VisibilityGraph {
+        val polygons = areaMap.polygons.map { it.normalize() }
+        val visibleNeighbours = polygons.flatMap { it.points }.associateWith { mutableSetOf<Point>() }
 
-    private fun intersects(p11: Point, p12: Point, p21: Point, p22: Point): Boolean {
-        val t1 = vectorMultiplication(p22, p12, p11)
-        val t2 = vectorMultiplication(p21, p12, p11)
-        val d1 = vectorMultiplication(p22, p12, p21)
-        val d2 = vectorMultiplication(p22, p11, p21)
-        return t1 * t2 <= 0 && d1 * d2 <= 0
-    }
+        val polygonPoints = polygons.associateWith { it.getConvexPoints() }
+        for (pts1 in polygonPoints.values) {
+            println("$pts1")
+            for ((i, p1) in pts1.withIndex()) {
+                val p1l = pts1[getPrevIndex(i, pts1)]
+                val p1r = pts1[getNextIndex(i, pts1)]
+                for (pts2 in polygonPoints.values) {
+                    for (p2 in pts2) {
+                        if (p1 == p2) continue
+                        if (p2.isInsideObservationArea(p1l, p1, p1r)) continue
 
-    private fun degreesBetween(pl: Point, p: Point, pr: Point): Double {
-        val v1 = Vector(p, pl)
-        val v2 = Vector(p, pr)
-        return acos((v1 dot v2) / (v1.length() * v2.length()))
-    }
+                        var foundIntersection = false
+                        for ((p3, p4) in polygons.asSequence().flatMap { it.lines() }) {
+                            if (setOf(p1, p2, p3, p4).size != 4) continue
 
-    private fun Polygon.getConvexPoints(): List<Point> {
-        return this.points // TODO
-    }
+                            if (intersects(p1, p2, p3, p4)) {
+                                foundIntersection = true
+                                println("($p1, $p2) intersects ($p3, $p4)")
+                                break
+                            }
+                        }
 
-    private fun Point.isInsideObservationArea(pl: Point, p: Point, pr: Point): Boolean {
-        return false // TODO
-    }
-
-    private fun Polygon.linesSequence() = sequence {
-        if (points.size == 1) return@sequence
-        for (i in points.indices) {
-            val p1 = points[i]
-            val p2 = points[floorMod(i + 1, points.size)]
-            yield(Pair(p1, p2))
-        }
-    }
-
-    private fun AreaMap.linesToCheckSequence() = sequence {
-        val areaMap = this@linesToCheckSequence
-
-        for (polygon1 in areaMap.polygons) {
-            val polygon1Points = polygon1.points
-
-            for ((i, p1) in polygon1Points.withIndex()) {
-                val pl = polygon1Points[floorMod(i - 1, polygon1Points.size)]
-                val pr = polygon1Points[floorMod(i + 1, polygon1Points.size)]
-
-                for (polygon2 in areaMap.polygons) {
-                    val polygon2Points = if (polygon1 == polygon2) polygon2.points else polygon2.getConvexPoints()
-
-                    for (p2 in polygon2Points) {
-                        if (p2.isInsideObservationArea(pl, p1, pr)) continue
-
-                        if (p1 != p2) yield(Pair(p1, p2))
+                        if (!foundIntersection) {
+                            visibleNeighbours[p1]?.add(p2)
+                            visibleNeighbours[p2]?.add(p1)
+                        }
                     }
                 }
             }
         }
-    }
 
-    override fun convertToVisibilityGraph(areaMap: AreaMap): VisibilityGraph {
-        val visibleNeighbours = areaMap.polygons.flatMap { it.points }.associateWith { mutableSetOf<Point>() }
-        outerLoop@ for ((p1, p2) in areaMap.linesToCheckSequence()) {
-            for ((p3, p4) in areaMap.polygons.asSequence().flatMap { it.linesSequence() }) {
-                if (setOf(p1, p2, p3, p4).size != 4) continue
-
-                if (intersects(p1, p2, p3, p4)) {
-                    continue@outerLoop
-                }
-            }
-            logger.trace("$p1,$p2 +")
-            visibleNeighbours[p1]!!.add(p2)
-            visibleNeighbours[p2]!!.add(p1)
-        }
         return visibleNeighbours.toVisibilityGraph()
     }
 }
--- visible-graph-finder/src/test/kotlin/com/a6raywa1cher/graph/GraphServiceImplTest.kt
+++ visible-graph-finder/src/test/kotlin/com/a6raywa1cher/graph/GraphServiceImplTest.kt
@@ -2,9 +2,38 @@ package com.a6raywa1cher.graph
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class GraphServiceImplTest {
     private val graphService = GraphServiceImpl()
+
+    @Test
+    fun testIsClockwise() {
+        val p1 = Polygon(
+            listOf(
+                Point(3, 1),
+                Point(3, 3),
+                Point(2, 2)
+            )
+        )
+        assertTrue(p1.isClockwise())
+    }
+
+    @Test
+    fun testIsInsideObservationArea() {
+        val pl = Point(3, 0)
+        val p = Point(0, 0)
+        val pr = Point(0, 3)
+        assertTrue(Point(1, 1).isInsideObservationArea(pl, p, pr))
+        assertTrue(Point(2, 1).isInsideObservationArea(pl, p, pr))
+        assertTrue(Point(3, 1).isInsideObservationArea(pl, p, pr))
+        assertTrue(Point(4, 1).isInsideObservationArea(pl, p, pr))
+        assertFalse(Point(-1, -1).isInsideObservationArea(pl, p, pr))
+        assertFalse(Point(2, -1).isInsideObservationArea(pl, p, pr))
+        assertFalse(pl.isInsideObservationArea(pl, p, pr))
+        assertFalse(pr.isInsideObservationArea(pl, p, pr))
+    }
 
     @Test
     fun testEmpty() {
@@ -135,19 +164,19 @@ class GraphServiceImplTest {
 
         val expected = VisibilityGraph(
             listOf(
-                Point(3, 1),
-                Point(3, 3),
-                Point(2, 2),
-                Point(5, 1),
-                Point(5, 3),
-                Point(6, 2)
+                Point(x = 2, y = 2),
+                Point(x = 3, y = 3),
+                Point(x = 3, y = 1),
+                Point(x = 5, y = 1),
+                Point(x = 5, y = 3),
+                Point(x = 6, y = 2)
             ),
             mapOf(
-                0 to setOf(2, 1, 4, 3),
-                1 to setOf(2, 0, 3, 4),
-                2 to setOf(0, 1),
-                3 to setOf(0, 1, 4, 5),
-                4 to setOf(3, 0, 1, 5),
+                0 to setOf(2, 1),
+                1 to setOf(0, 2, 3, 4),
+                2 to setOf(0, 1, 3, 4),
+                3 to setOf(2, 1, 4, 5),
+                4 to setOf(1, 2, 3, 5),
                 5 to setOf(3, 4)
             )
         )
@@ -163,16 +192,16 @@ class GraphServiceImplTest {
                     listOf(
                         Point(0, 0),
                         Point(0, 1),
+                        Point(1, 1),
                         Point(1, 0),
-                        Point(1, 1)
                     )
                 ),
                 Polygon(
                     listOf(
                         Point(10, 0),
                         Point(10, 1),
+                        Point(11, 1),
                         Point(11, 0),
-                        Point(11, 1)
                     )
                 )
             )
@@ -181,24 +210,24 @@ class GraphServiceImplTest {
 
         val expected = VisibilityGraph(
             listOf(
-                Point(0, 0),    // 0
-                Point(0, 1),    // 1
-                Point(1, 0),    // 2
-                Point(1, 1),    // 3
-                Point(10, 0),   // 4
-                Point(10, 1),   // 5
-                Point(11, 0),   // 6
-                Point(11, 1)    // 7
+                Point(0, 0),
+                Point(0, 1),
+                Point(1, 1),
+                Point(1, 0),
+                Point(10, 0),
+                Point(10, 1),
+                Point(11, 1),
+                Point(11, 0),
             ),
             mapOf(
-                0 to setOf(1, 2),
+                0 to setOf(2, 1),
                 1 to setOf(0, 3),
                 2 to setOf(0, 3, 4, 5),
-                3 to setOf(1, 2, 4, 5),
+                3 to setOf(2, 1, 4, 5),
                 4 to setOf(2, 3, 5, 6),
                 5 to setOf(4, 2, 3, 7),
                 6 to setOf(4, 7),
-                7 to setOf(5, 6)
+                7 to setOf(6, 5)
             )
         )
 
